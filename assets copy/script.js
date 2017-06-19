260439945fe3e7044c09dcfbb0cbc9085384afe0
Noah Sokoloff <N.W.Sokoloff@gmail.com>
2017-06-19 15:05:26 -0400

    pages layout

---
+++ assets copy/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+  /* hamburger glyphicon */
+
+  var menu = $(".menu");
+  var hamburger = $(".hamburger");
+  var line = $(".line");
+  var menuOpen;
+
+  function openMenu(){
+    menu.css("left", "0px");
+    line.css("background", "##8A828A");
+    menuOpen = true;
+  }
+
+  function closeMenu(){
+    menu.css("left", "-520px");
+    line.css("background", "#8A828A");
+    menuOpen = false;
+  }
+
+  function toggleMenu(){
+    if (menuOpen) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  }
+
+  hamburger.on({
+    mouseenter: function(){
+      openMenu();
+    }
+  });
+
+  menu.on({
+    mouseleave: function(){
+      closeMenu();
+    }
+
+  });
+
+  hamburger.on({
+    click: function(){
+      toggleMenu();
+    }
+  })
+
+});
