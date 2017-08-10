3962451cbf5726f3fc4c704b41e9485291cf036e
Noah Sokoloff <N.W.Sokoloff@gmail.com>
2017-08-10 11:48:34 -0400

    rehab

--- assets/script.js
+++ assets/script.js
@@ -95,6 +95,7 @@ if (modal) {
   close.onclick = function() {
     modal.style.display = "none";
   }
+}
 
 
 // When the user clicks anywhere outside of the modal, close it
@@ -102,5 +103,5 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+  }
 });
